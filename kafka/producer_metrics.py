7122b685ff5636bf9d65e673746f29d83ec67f4c
juanes0124 <conto8.1juanesv@gmail.com>
2026-05-27 15:39:35 -0500

    Update Kafka producer and consumer for real-time metrics

--- kafka/producer_metrics.py
+++ kafka/producer_metrics.py
@@ -4,10 +4,27 @@ import pandas as pd
 import json
 import time
 
-engine = create_engine(
-    "postgresql+psycopg2://cristiancolorado@localhost:5432/etl_accidentes"
-)
+def get_engine():
+    user = "etl_user"
+    password = "1234"
+    host = "localhost"
+    port = "5434"
+    database = "etl_accidentes"
 
+    engine = create_engine(
+        f"postgresql+psycopg2://{user}:{password}@{host}:{port}/{database}",
+        connect_args={
+            "options": "-c search_path=etl,public"
+        },
+        future=True
+    )
+
+    return engine
+
+
+engine = get_engine()
+
+# 🚀 KAFKA PRODUCER
 producer = KafkaProducer(
     bootstrap_servers="localhost:9092",
     value_serializer=lambda v: json.dumps(v).encode("utf-8")
@@ -17,53 +34,46 @@ print("🚀 Producer iniciado")
 
 while True:
 
+    # 📊 TOTAL ACCIDENTES
     total = pd.read_sql(
-        """
-        SELECT COUNT(*) total
-        FROM fact_accidentes
-        """,
+        "SELECT COUNT(*) AS total FROM fact_accidentes",
         engine
     ).iloc[0]["total"]
 
+    # 🌧️ ACCIDENTES CON LLUVIA
     lluvia = pd.read_sql(
         """
-        SELECT COUNT(*) total
+        SELECT COUNT(*) AS total
         FROM fact_accidentes f
-        JOIN dim_clima c
-        ON f.clima_id = c.clima_id
+        JOIN dim_clima c ON f.clima_id = c.clima_id
         WHERE c.precipitacion > 0
         """,
         engine
     ).iloc[0]["total"]
 
+    # 🌡️ TEMPERATURA PROMEDIO
     temperatura = pd.read_sql(
         """
-        SELECT ROUND(
-            AVG(temperatura)::numeric,
-            2
-        ) temperatura
+        SELECT COALESCE(ROUND(AVG(temperatura)::numeric, 2), 0)
+        AS temperatura
         FROM dim_clima
         """,
         engine
     ).iloc[0]["temperatura"]
 
+    # 📦 MENSAJE KAFKA
     mensaje = {
-        "timestamp": time.strftime(
-            "%Y-%m-%d %H:%M:%S"
-        ),
+        "timestamp": time.strftime("%Y-%m-%d %H:%M:%S"),
         "total_accidentes": int(total),
         "accidentes_lluvia": int(lluvia),
         "temperatura_promedio": float(temperatura)
     }
 
-    producer.send(
-        "accidentes_metrics",
-        mensaje
-    )
-
+    # 📤 ENVIAR A KAFKA
+    producer.send("accidentes_metrics", mensaje)
     producer.flush()
 
-    print("\n📤 Mensaje enviado")
+    print("\n📤 Mensaje enviado:")
     print(mensaje)
 
-    time.sleep(10)
+    time.sleep(10)  
